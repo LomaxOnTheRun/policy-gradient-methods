94d4af5a8168513f8438e3d103cbed2026817f2b
Ben Lomax <lomax.on.the.run@gmail.com>
2020-05-24 17:41:00 +0300

    Add logic to update_policy, still need NN

--- policy_gradient.py
+++ policy_gradient.py
@@ -1,7 +1,9 @@
 import gym
 import matplotlib.pyplot as plt
+import numpy as np
 
 MAX_EPISODES = 10
+GAMMA = 0.9
 
 # Environment solved at 195 steps for 100 consecutive episodes
 SUCCESS_EPISODES = 100
@@ -17,22 +19,38 @@ def get_action(policy, state, env):
     return env.action_space.sample()  # Random action
 
 
-def get_probability(policy, state, action):
+def get_action_probability(policy, state, action):
     """
     Get the probability of selecting the given action given the current policy.
 
     TODO: Use NN for this.
     """
-    pass
+    return 1e-10
 
 
-def update_policy(policy, rewards, probabilities):
+def update_policy(policy, rewards, action_probs):
     """
     Update the policy with rewards gained throughout episode.
 
-    TODO: Actually do this
+    Discounted rewards are a sum of all future rewards, where the further into the
+    future a reward is, the more it's discounted:
+
+        G_t = Σ_t γ^t * r_t
+
+    TODO: Upgrade NN.
     """
-    pass
+    num_steps = len(rewards)
+    policy_gradient = []
+    for t in range(num_steps):
+        # Calculate discounted reward
+        discounted_reward = 0
+        for i, reward in enumerate(rewards[t:]):
+            discounted_reward += (GAMMA ** i) * reward
+
+        gradient = -discounted_reward * np.log(action_probs[t])
+        policy_gradient.append(gradient)
+
+    # TODO: Upgrade NN
 
 
 env = gym.make("CartPole-v0")
@@ -46,21 +64,20 @@ episode_steps = []
 for episode in range(MAX_EPISODES):
     state = env.reset()
     rewards = []
-    probabilities = []
+    action_probs = []
 
     for step in range(SUCCESS_STEPS + 1):
         # env.render()
-
         action = get_action(policy, state, env)
-        probability = get_probability(policy, state, action)
+        action_prob = get_action_probability(policy, state, action)
 
         state, reward, done, _ = env.step(action)
 
         rewards.append(reward)
-        probabilities.append(probability)
+        action_probs.append(action_prob)
 
         if done:
-            update_policy(policy, rewards, probabilities)
+            update_policy(policy, rewards, action_probs)
             episode_steps.append(step)
             print(f"Episode {episode + 1}:\tAgent lasted {step} steps")
             break
